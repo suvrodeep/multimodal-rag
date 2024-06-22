a393244a8e723941b16d52401a42b174cedbdf28
Suvrodeep Ghosh <suvrodeep.ghosh@outlook.com>
2024-06-22 03:29:54 +0200

    element extraction

--- src/extract_doc_elements.py
+++ src/extract_doc_elements.py
@@ -1,48 +1,62 @@
-from langchain_text_splitters import CharacterTextSplitter
 from unstructured.partition.pdf import partition_pdf
-from unstructured.documents.elements import Element, ElementMetadata, ElementType, Text, Image
 import os
-from typing import List
 
 
-# Categorize elements by type
-def categorize_elements(raw_pdf_elements: List[List[Element]]) -> (List[str], List[dict[str, str]]):
-    """
-    Categorize extracted elements from a PDF into tables and texts.
-    raw_pdf_elements: List of lists of unstructured.documents.elements
-    """
-    tables = []
-    texts = []
-    for pdf_elements in raw_pdf_elements:
-        for element in pdf_elements:
-            if "unstructured.documents.elements.Table" in str(type(element)):
-                tables.append({'raw_text': element.text,
-                               'text_as_html': element.metadata.to_dict()['text_as_html']})
-            elif "unstructured.documents.elements.CompositeElement" in str(type(element)):
-                texts.append(element.text)
-    return texts, tables
+class ElementExtractor:
+    def __init__(self, input_dir: str, image_dir: str, chunk_size: int = None):
+        self.input_dir = input_dir
+        self.image_dir = image_dir
+        self.raw_elements = []
+        self.raw_texts = []
+        self.raw_tables = []
 
+        if chunk_size is None:
+            self.chunk_size = 4096
+        else:
+            self.chunk_size = chunk_size
 
-# File path
-input_path = "../data/input"
-image_path = "../data/image"
+    # Get raw elements
+    def get_raw_elements(self):
+        """
+        Get raw elements from PDF
+        :return:
+        """
+        # Get elements
+        file_paths = [self.input_dir + '/' + file for file in os.listdir(self.input_dir)
+                      if file.split(".")[-1] == 'pdf']
+        print(f'Identified {len(file_paths)} PDF files for processing')
 
-# Get elements
-file_paths = [input_path + '/' + file for file in os.listdir(input_path) if file.split(".")[-1] == 'pdf']
-raw_pdf_elements = []
-for file_path in file_paths:
-    raw_pdf_elements.append(partition_pdf(filename=file_path,
-                                          chunking_strategy='by_title',
-                                          max_characters=4096,
-                                          extract_image_block_types=['Image'],
-                                          extract_image_block_output_dir=image_path,
-                                          infer_table_structure=True))
+        for file_path in file_paths:
+            self.raw_elements.append(partition_pdf(filename=file_path,
+                                                   chunking_strategy='by_title',
+                                                   max_characters=self.chunk_size,
+                                                   extract_image_block_types=['Image'],
+                                                   extract_image_block_output_dir=self.image_dir,
+                                                   infer_table_structure=True))
 
-# Get text, tables
-texts, tables = categorize_elements(raw_pdf_elements)
+    # Categorize elements by type
+    def categorize_elements(self):
+        """
+        Categorize extracted elements from a PDF into tables and texts.
+        raw_pdf_elements: List of lists of unstructured.documents.elements
+        :return:
+        """
 
-# Optional: Enforce a specific token size for texts
-text_splitter = CharacterTextSplitter.from_tiktoken_encoder(chunk_size=4096, chunk_overlap=0)
+        for pdf_elements in self.raw_elements:
+            for element in pdf_elements:
+                if "unstructured.documents.elements.Table" in str(type(element)):
+                    table_metadata = element.metadata.to_dict()
+                    self.raw_tables.append({'raw_text': element.text,
+                                            'text_as_html': table_metadata['text_as_html'],
+                                            'doc_path': table_metadata['file_directory'] + '/' + table_metadata[
+                                                'filename']
+                                            })
+                elif "unstructured.documents.elements.CompositeElement" in str(type(element)):
+                    text_metadata = element.metadata.to_dict()
+                    self.raw_texts.append({'raw_text': element.text,
+                                           'doc_path': text_metadata['file_directory'] + '/' + text_metadata['filename']
+                                           })
 
-joined_texts = " ".join(texts)
-texts_4k_token = text_splitter.split_text(joined_texts)
+        print(f'Got {len(self.raw_texts)} text elements')
+        print(f'Got {len(self.raw_tables)} table elements')
+
---
+++ src/tokenizer.py
@@ -0,0 +1,32 @@
+from langchain_text_splitters import CharacterTextSplitter
+from typing import List, Dict
+
+
+class Tokenizer:
+    def __init__(self, raw_texts: List[Dict[str, str]], chunk_size: int = None, chunk_overlap: int = None):
+        if chunk_size is None:
+            self.chunk_size = 2048
+        else:
+            self.chunk_size = chunk_size
+
+        if chunk_overlap is None:
+            self.chunk_overlap = 0
+        else:
+            self.chunk_overlap = chunk_overlap
+
+        self.tokens = []
+        self.raw_texts = raw_texts
+
+    def generate_tokens(self):
+        # Optional: Enforce a specific token size for texts
+        text_splitter = CharacterTextSplitter.from_tiktoken_encoder(chunk_size=self.chunk_size, chunk_overlap=0)
+
+        # Generate tokens per doc
+        doc_paths = set([item['doc_path'] for item in self.raw_texts])
+        for doc in doc_paths:
+            joined_texts = " ".join([text['raw_text'] for text in self.raw_texts if text['doc_path'] == doc])
+            tokens = text_splitter.split_text(joined_texts)
+            self.tokens.append({'tokens': tokens,
+                                'text': joined_texts,
+                                'doc_path': doc
+                                })
